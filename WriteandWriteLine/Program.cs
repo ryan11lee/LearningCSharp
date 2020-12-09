0a99f85427df6da9364e49ae7eb973401c3162af
Ryan Lee <45303876+ryan11lee@users.noreply.github.com>
2020-12-08 22:46:54 -0800

    Writeline and console entry

---
+++ WriteandWriteLine/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace WriteandWriteLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Hellow Welcome");
+        Console.Write("Hello");
+        Console.Write("Welcome");
+        Console.WriteLine("Enter a string and press enter");
+        string readInput = Console.ReadLine();
+        Console.WriteLine("You have entered {0}",readInput);
+
+        Console.Write("Enter a strong and press enter");
+        int asciiValue = Console.Read();
+        Console.WriteLine("ASCII value is {0}", asciiValue);
+        Console.ReadKey();
+        }
+    }
+}
